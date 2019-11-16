d82bcdceb76311df2d25a52f1cf79e1246df9a03
coryhighsea <cjhisey@gmail.com>
2019-11-16 15:37:29 -0700

    added a hover effect for the nav bar

---
+++ jQuery.js
@@ -0,0 +1,17 @@
+$("li").on("mouseenter", function() {
+    $(this).css("font-weight", "bold")
+});
+
+$("li").on("mouseleave", function() {
+    $(this).css('font-weight', 'normal')
+
+});
+
+$(".navbar-brand").on("mouseenter", function() {
+    $(this).css("font-weight", "bold")
+});
+
+$(".navbar-brand").on("mouseleave", function() {
+    $(this).css('font-weight', 'normal')
+
+});
