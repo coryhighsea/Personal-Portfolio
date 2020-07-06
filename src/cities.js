6d75ace58c1dda66db5383b78b769699dbbd327f
Cory Hisey <cjhisey@gmail.com>
2020-07-06 12:41:03 -0700

    Formatting

--- src/cities.js
+++ src/cities.js
@@ -1,7 +1,7 @@
 var london = L.marker([51.5, -0.09]).bindPopup('<strong>London<strong><br><img width="200px" src="images/london1.jpg">'),
-	brussels    = L.marker([50.8, 4.36]).bindPopup('Brussels'),
-	berlin    = L.marker([52.5, 13.4]).bindPopup('Berlin'),
-    cologne    = L.marker([50.923, 6.94]).bindPopup('Cologne<br><img width="200px" src="images/dom.jpg">'),
+    brussels = L.marker([50.8, 4.36]).bindPopup('Brussels'),
+    berlin = L.marker([52.5, 13.4]).bindPopup('Berlin'),
+    cologne = L.marker([50.923, 6.94]).bindPopup('Cologne<br><img width="200px" src="images/dom.jpg">'),
     amsterdam = L.marker([52.3699, 4.8982]).bindPopup('<b>Amsterdam</b><br><img width="200px" src="images/amsterdam (1).jpg">'),
     pinchercreek = L.marker([49.31, -114.41]).bindPopup('<b>Castle Mountain</b><br><img width="200px" src="https://lh3.googleusercontent.com/nYt8bv_sNCc2zVQ3cW_7AN1N_CXfa46uF_CKpa2XnH7PXvu6IUEfh6cJVuDP5nl-UJaOaLbRu7MTDT2k5T2UsUDsHoyt0khy5o6T-H4hntdoeXzhomdxPZ3LN5MlD5THvASzQgAiEK_S7RPVj9joIkMNkaHcgoHXHxusoaWpeUvUOBPfPZs60iuHAkK-fAuJOLpvdjq-7Vau3LnW-_Yjdippzjew_hkPu2YLhV02xx1Tx0guSPXAsG90FVNolel4H3L-T0s1TYSjfTvyIpx60C7QSe3QcJypsODO012VR5npjyaMyasfnm7WTbD_vv1xg976GoKRJtAVZimqr7FK6BEddQSTrCheJLv8zFiKcCRSBC_SSv3LIiaicP7Kknvi0srNRKCl6x8eGz3btVObAPxknW0W3pqqlpzrNLmEFcX5MUA5Xg3YNPBOEUovirf5Xhl969kiqtPGzVr442TP9SeJvxPDDAmucGaD7tQeS3SeaZG5AyXlzDp2U-wodxDT3RiiFFuUAOvZlNm0qjWeAnu7djZ4MInxfLsH2TJ0QMqrcSvyhVHC28gKKrCIrHRm4usPE-z2tW_LFdiheACgpQVVcVk-srMGcx59KcSYtd99DtogS3di_drh1AS05p8XPfqk8lO6j_nHKEbFkrK6hnKQCY11EEFHO7bQUVvQrFbz2fMS-UHxnY5P=w493-h657-no">'),
     dresden = L.marker([51.06, 13.74]).bindPopup('Dresden'),
@@ -26,11 +26,11 @@ var london = L.marker([51.5, -0.09]).bindPopup('<strong>London<strong><br><img w
     stevenage = L.marker([51.9, -0.263]).bindPopup('Stevenage'),
     bangkok = L.marker([13.72, 100.35]).bindPopup('Bangkok'),
     phuket = L.marker([7.83, 98.4]).bindPopup('Phuket'),
-    calgary = L.marker([51, -114]).bindPopup('Calgary'),    
+    calgary = L.marker([51, -114]).bindPopup('Calgary'),
     reddeer = L.marker([52.3, -113.8]).bindPopup('Red Deer'),
     edmonton = L.marker([53.5, -113.5]).bindPopup('Edmonton'),
     vancouver = L.marker([49.2, -123]).bindPopup('Vancouver'),
-    kelowna = L.marker([49.9, -119.5]).bindPopup('Kelowna');
+    kelowna = L.marker([49.9, -119.5]).bindPopup('Kelowna'),
     marrakech = L.marker([31.631661, -7.988271]).bindPopup('<b>Marrakech</b><br><img width="200px" src="images/marrakech.jpg">'),
     paris = L.marker([48.858797, 2.356567]).bindPopup('Paris'),
     la = L.marker([34.010944, -118.226074]).bindPopup('Los Angeles'),
@@ -58,8 +58,8 @@ var london = L.marker([51.5, -0.09]).bindPopup('<strong>London<strong><br><img w
 
 
 
-    var cities = L.layerGroup([london, brussels, berlin, cologne, amsterdam, pinchercreek, dresden, prague, munich, vienna, venice, bern,
-         verona, barcelona, madrid, nuremburg, frankfurt, hamburg, warsaw, dusseldorf, budapest, miskolc, istanbul, athens, agistri, stevenage, 
-        bangkok, phuket, calgary, reddeer, edmonton, vancouver, kelowna, marrakech, paris, la, seattle, cancun, playa, miami, tampa, houston, phoenix, toronto,
-    ottawa, montreal, quebec, halifax, charlottetown, orlando, regina, winnipeg, saskatoon, thunderBay, lasVegas, keyWest]);
-    
+var cities = L.layerGroup([london, brussels, berlin, cologne, amsterdam, pinchercreek, dresden, prague, munich, vienna, venice, bern,
+    verona, barcelona, madrid, nuremburg, frankfurt, hamburg, warsaw, dusseldorf, budapest, miskolc, istanbul, athens, agistri, stevenage,
+    bangkok, phuket, calgary, reddeer, edmonton, vancouver, kelowna, marrakech, paris, la, seattle, cancun, playa, miami, tampa, houston, phoenix, toronto,
+    ottawa, montreal, quebec, halifax, charlottetown, orlando, regina, winnipeg, saskatoon, thunderBay, lasVegas, keyWest
+]);
--- src/intoMap.js
+++ src/intoMap.js
@@ -1,157 +1,158 @@
 var mymap = L.map('mapid').setView([51.505, 6], 5);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	id: 'mapbox.streets'
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
 }).addTo(mymap);
 
 // L.marker([50.941326, 6.957994]).addTo(mymap).bindPopup('Cologne');
 var domIcon = L.icon({
     iconUrl: 'dom.svg',
 
-    iconSize:     [60, 80], // size of the icon
-    iconAnchor:   [30, 40], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -35] // point from which the popup should open relative to the iconAnchor
+    iconSize: [60, 80], // size of the icon
+    iconAnchor: [30, 40], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -35] // point from which the popup should open relative to the iconAnchor
 });
 
-L.marker([50.941326, 6.957994], {icon: domIcon}).addTo(mymap).bindPopup('<img width="200px" src="/images/dom.jpg">');
+L.marker([50.941326, 6.957994], { icon: domIcon }).addTo(mymap).bindPopup('<img width="200px" src="/images/dom.jpg">');
 
 var europeancountriesGeoJSON = false;
 fetch('/europeancountries.json', {
-    method: 'GET'
-})
-.then(response => response.json())
-.then(json => {
-    var geojson = L.geoJSON(json, {
-        style: function (feature){
-            return {
-                fillOpacity: 0,
-                weight: 0.3,
+        method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+        var geojson = L.geoJSON(json, {
+            style: function(feature) {
+                return {
+                    fillOpacity: 0,
+                    weight: 0.3,
+                }
+            },
+            onEachFeature: function(feature, layer) {
+                layer.on('mouseover', function() {
+                    layer.setStyle({ fillOpacity: 0.3 })
+                })
+                layer.on('mouseout', function() {
+                    layer.setStyle({ fillOpacity: 0 })
+                })
+                layer.on('click', function(e) {
+                    mymap.fitBounds(e.target.getBounds())
+                })
             }
-        },
-        onEachFeature: function(feature, layer) {
-            layer.on('mouseover', function(){
-                layer.setStyle({fillOpacity: 0.3})
-            })
-            layer.on('mouseout', function(){
-                layer.setStyle({fillOpacity: 0})
-            })
-			layer.on('click', function(e){
-				mymap.fitBounds(e.target.getBounds())
-			})
-        }
-    }).addTo(mymap);
-})
-.catch(error => console.log(error.message));
+        }).addTo(mymap);
+    })
+    .catch(error => console.log(error.message));
 
 // States map
-	
+
 var map = L.map('map').setView([37.8, -96], 3);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>', 		maxZoom: 18,
-		id: 'mapbox.light', 		// id: 'mapbox.satellite',
-	   accessToken: 'pk.eyJ1IjoiZHRjMiIsImEiOiJjajM0bGhtdWMwMXMyMndvOTNhMTV5bGlwIn0.P982OmG7OY8ETeL971l1Ww'
-	}).addTo(map);
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.light', // id: 'mapbox.satellite',
+    accessToken: 'pk.eyJ1IjoiZHRjMiIsImEiOiJjajM0bGhtdWMwMXMyMndvOTNhMTV5bGlwIn0.P982OmG7OY8ETeL971l1Ww'
+}).addTo(map);
 
 
 L.geoJSON(statesData).addTo(map);
+
 function getColor(d) {
-   return d > 1000 ? 'green' :
-		  d > 500  ? 'orange' :
-		  d > 200  ? 'red' :
-		  d > 100  ? 'blue' :
-		  d > 50   ? 'lightgreen' :
-		  d > 20   ? 'lightblue' :
-		  d > 10   ? 'yellow' :
-					 'purple';
+    return d > 1000 ? 'green' :
+        d > 500 ? 'orange' :
+        d > 200 ? 'red' :
+        d > 100 ? 'blue' :
+        d > 50 ? 'lightgreen' :
+        d > 20 ? 'lightblue' :
+        d > 10 ? 'yellow' :
+        'purple';
 }
 
 function style(feature) {
-   return {
-	   fillColor: getColor(feature.properties.density),
-	   weight: 2,
-	   opacity: 1,
-	   color: 'white',
-	   dashArray: '3',
-	   fillOpacity: 0.7
-   };
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
 }
-L.geoJson(statesData, {style: style}).addTo(map);
+L.geoJson(statesData, { style: style }).addTo(map);
 
 function highlightFeature(e) {
-   var layer = e.target;
-   info.update(layer.feature.properties);
+    var layer = e.target;
+    info.update(layer.feature.properties);
 
-   layer.setStyle({
-	   weight: 5,
-	   color: '#666',
-	   dashArray: '',
-	   fillOpacity: 0.7
-   });
-   
-   if(!L.Browser.ie && !L.Browser.opera && !L.Browser.edge){
-	   layer.bringToFront();
-   }
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
 }
 
 function resetHighlight(e) {
-   geojson.resetStyle(e.target);
-   info.update();
+    geojson.resetStyle(e.target);
+    info.update();
 }
 
 
 function zoomToFeature(e) {
-   map.fitBounds(e.target.getBounds());
+    map.fitBounds(e.target.getBounds());
 }
 
 function onEachFeature(feature, layer) {
-   layer.on({
-	   mouseover: highlightFeature,
-	   mouseout: resetHighlight,
-	   click: zoomToFeature
-   });
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
 }
 
 geojson = L.geoJson(statesData, {
-   style: style,
-   onEachFeature: onEachFeature
+    style: style,
+    onEachFeature: onEachFeature
 }).addTo(map);
 
 
 var info = L.control();
 
-info.onAdd = function (map) {
-   this._div = L.DomUtil.create('div', 'info');
-   this.update();
-   return this._div;
+info.onAdd = function(map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
 };
 
-info.update = function (props) {
-   this._div.innerHTML = '<h4>US States</h4>' + (props ?
-   '<b>' + props.name + '</b><br />' 
-   : 'Hover over a state');
+info.update = function(props) {
+    this._div.innerHTML = '<h4>US States</h4>' + (props ?
+        '<b>' + props.name + '</b><br />' :
+        'Hover over a state');
 }
 
 info.addTo(map);
 
 
 
-var legend = L.control({position: 'bottomright'});
+var legend = L.control({ position: 'bottomright' });
 
-legend.onAdd = function (map) {
+legend.onAdd = function(map) {
 
-   var div = L.DomUtil.create('div', 'info legend'),
-	   grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-	   labels = [];
-   for (var i = 0; i < grades.length; i++){
-	   div.innerHTML += 
-			   '<i style="background:' + getColor(grades[i] +1) + '"></i> ' +
-			   grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-   }
-   return div;
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
 };
-
